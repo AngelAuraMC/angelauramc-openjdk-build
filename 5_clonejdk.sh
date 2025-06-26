4ec1bcf52347a0a0795e88191fa277ee30dfaa06
Eva Isabella Luna <me@crystall1ne.dev>
2025-06-25 22:11:12 -0600

    [FIX] Downgrade Android JDK to bypass the compile errors
    TODO: Update to JDK 21.0.8+7 when I have time.

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -2,7 +2,12 @@
 set -e
 
 if [[ $TARGET_VERSION -eq 21 ]]; then
-    git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+    if [[ $BUILD_IOS ]]; then
+        git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+    else
+        # TODO: Update Android to JDK 21.0.8+7
+        git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+    fi
 else
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
 fi
