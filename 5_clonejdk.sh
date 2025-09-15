d2f4afdee5f45495eb44841f83fb9cc6724d4f0f
Eva Isabella Luna <me@crystall1ne.dev>
2025-09-15 16:24:25 -0600

    Temporarily downgrade JDK 17 for iOS
    Signed-off-by: Eva Isabella Luna <me@crystall1ne.dev>

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -9,5 +9,9 @@ if [[ $TARGET_VERSION -eq 21 ]]; then
         git clone --branch jdk21.0.1 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     fi
 else
-    git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    if [[ $BUILD_IOS ]]; then
+        git clone --branch jdk-17.0.17+1 --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    else
+        git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
+    fi
 fi
