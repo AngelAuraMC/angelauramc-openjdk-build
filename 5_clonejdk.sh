3f41bcabf703fc5bfd75492b0ed713a73dd86541
khanhduytran0 <khanhduytran0@users.noreply.github.com>
2026-07-10 14:01:03 +0700

    add: java 25

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-if [[ $TARGET_VERSION -eq 21 ]]; then
+if [[ $TARGET_VERSION -eq 25 ]]; then
+    git clone --depth 1 https://github.com/openjdk/jdk25u openjdk-25
+elif [[ $TARGET_VERSION -eq 21 ]]; then
     if [[ $BUILD_IOS ]]; then
         git clone --branch jdk-21.0.8+7 --depth 1 https://github.com/openjdk/jdk21u openjdk-21
     else
